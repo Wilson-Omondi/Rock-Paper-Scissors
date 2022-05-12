47742d2875b21435341d64d4fa0c35d419f96898
Wilson Omondi <wangaw94@gmail.com>
2022-05-12 10:44:35 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// function game (player1, player2){
+
+//   if(player1 == 'scissors' && player2 == 'paper'){
+//     return 'Player 1 won!';
+//   }else if(player1 == 'scissors' && player2 == 'rock'){
+//     return 'Player 1 won!';
+//   }else if(player1 == 'paper' && player2 == 'paper' || player1 == 'rock' && player2 == 'rock' || player1 == 'scissors' && player2 == 'scissors'){
+//     return 'Draw!';
+//   }
+// }
+
+// console.log(game('rock', 'rock'));
+
+//const num = Math.floor(Math.random() *3);
+
+// console.log(num);
+
+function computerPlay(){
+  let options = ['rock', 'paper', 'scissors'];
+
+  let num = Math.floor(Math.random() *3);
+
+  return options[num]
+}
+
+
+function playRound
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const computerSelection = computerPlay(); 
+
+//let playerSelection = prompt('Choose Either of Rock, Pepper or Scissors')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
